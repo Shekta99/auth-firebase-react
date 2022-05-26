0d33b355a0feac8cc6dd27fa703f6ac756ed40b1
Sergio Marentes <sergiomarentes705@gmail.com>
2022-05-26 17:33:13 -0500

    setup initial configs to auth with google and firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,27 @@
+import { app } from "../firebaseConfig";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
+
 function Home() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+
+    signInWithPopup(auth, provider)
+      .then((userCredentials) => {
+        console.log(`welcome ${userCredentials.user.displayName}`);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
   return (
     <>
       <h1>Página home</h1>
       <p>Bienvenidos a mi página web de ventas</p>
+      <button onClick={handleGoogleSignIn}>Sign in with google</button>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
     </>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxNh23-oqMvdGbKWPIneaSKaQve2neI3M",
+  authDomain: "authentication-d03b4.firebaseapp.com",
+  projectId: "authentication-d03b4",
+  storageBucket: "authentication-d03b4.appspot.com",
+  messagingSenderId: "78335983305",
+  appId: "1:78335983305:web:83062d5b0edb6b31eb1718"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
